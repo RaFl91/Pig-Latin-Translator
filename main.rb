88ec0b5273c97181a2c2f7d7c10e55eed05fa3b4
Rashad Flemings <terrellflemings@gmail.com>
2020-01-24 19:54:05 +0000

    built consonant cases pre-test

--- main.rb
+++ main.rb
@@ -11,8 +11,23 @@ def translate(str)
 	#if word starts with vowel
 		if vowels.include? word[0]
 			result.push word << "ae"
+	#if word starts with consonants
+		else
+		#three consonants
+			if three_consonants.include? word[0] + word[1] + word[2]
+				first_three = word.slice!(0,3)
+				result.push word << first_three << "ae"
+		#two consonants
+			elsif two_consonants.include? word[0] +word[1]
+				first_two = word.slice!(0,2)
+				result.push word << first_two << "ae"
+			else
+		#one consonant
+				first_letter = word.slice(0)
+				result.push word << first_letter << "ae" 
+			end
 		end
 	end
 
 end
-translate('air')
+translate()
